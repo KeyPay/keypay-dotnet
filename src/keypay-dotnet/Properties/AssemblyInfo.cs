6016a39ddf1be8b1b63f83b01c3f028456f14adf
ci <ci@webscale.com.au>
2020-11-11 11:46:52 +0000

    Buld 1.0.105 updates: To git creds in pipeline yml

--- src/keypay-dotnet/Properties/AssemblyInfo.cs
+++ src/keypay-dotnet/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("1.1.0.17-rc10")]
+[assembly: AssemblyInformationalVersion("1.0.105-DO-NOT-USE")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
