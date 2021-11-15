aeaa18c7ffbcaea5481945272fe15e1c0a9a1efa
Ben Taylor <bentaylortheonly@gmail.com>
2021-11-15 11:58:28 +0000

    update version to 1.1.0.17-rc13#

--- src/keypay-dotnet/Properties/AssemblyInfo.cs
+++ src/keypay-dotnet/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("1.1.0.17-rc12")]
+[assembly: AssemblyInformationalVersion("1.1.0.17-rc13")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
